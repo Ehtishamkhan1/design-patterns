0087a0e5989908d3b37f934c1fb29cb1372dcce0
Ehtisham khan <106956695+Ehtishamkhan1@users.noreply.github.com>
2023-12-07 09:29:32 +0500

    updated

---
+++ GOF/Fly weight/Flyweight/src/flyweight/Demo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flyweight;
+
+/**
+ *
+ * @author FA20-BSE-039
+ */
+import java.awt.Color;
+
+
+
+public class Demo {
+    static int CANVAS_SIZE = 500;
+    static int TREES_TO_DRAW = 100;
+    static int TREE_TYPES = 2;
+
+    public static void main(String[] args) {
+        Forest forest = new Forest();
+
+        // Planting trees
+        for (int i = 0; i < Math.floor(TREES_TO_DRAW / TREE_TYPES); i++) {
+            forest.plantTree(random(0, CANVAS_SIZE), random(0, CANVAS_SIZE),
+                    "Summer Oak", Color.GREEN, "Oak texture stub");
+            forest.plantTree(random(0, CANVAS_SIZE), random(0, CANVAS_SIZE),
+                    "Autumn Oak", Color.ORANGE, "Autumn Oak texture stub");
+        }
+
+        // Displaying the forest
+        forest.setSize(CANVAS_SIZE, CANVAS_SIZE);
+        forest.setVisible(true);
+
+        // Outputting the memory usage
+        System.out.println(TREES_TO_DRAW + " trees drawn");
+        System.out.println("---------------------");
+        System.out.println("Memory usage:");
+        System.out.println("Tree size (8 bytes) * " + TREES_TO_DRAW);
+        System.out.println("+ TreeTypes size (~30 bytes) * " + TREE_TYPES + "");
+        System.out.println("---------------------");
+        System.out.println("Total: " + ((TREES_TO_DRAW * 8 + TREE_TYPES * 30) / 1024 / 1024) +
+                "MB (instead of " + ((TREES_TO_DRAW * 38) / 1024 / 1024) + "MB)");
+    }
+
+    private static int random(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+}
---
+++ GOF/Fly weight/Flyweight/src/flyweight/Forest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flyweight;
+
+/**
+ *
+ * @author FA20-BSE-039
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
